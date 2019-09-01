508742f21e5dbac5f2fa4866c3880c3881ad3b83
offmarketeu <analisis@offmarket.eu>
2019-09-01 18:01:15 +0200

    Add files via upload

--- credit_plus.r
+++ credit_plus.r
@@ -1,21 +1,29 @@
+library(readxl)
 
-ur=1101401
+ur<-1101453
+ecu<- 'UK_CM_1'
+ca<- 0.145
+lgd_add<- 1.46
 
-# Parametros
+# datos credito
 
-ec <- read.csv2("file_ec")
-mc <- read.csv2("file_mc")
-ca <- read.csv2("file_ca")
-lgd_add<- read.csv2("file_lgd")
+fd<- read.csv2("E:/APP_OFFMARKET/credit_plus/UK_retail.csv")
+fd_ru<- fd[fd$RU_ID==ur,]
+
+# ecuaciones minoristas
+ec <- read_excel("E:/APP_OFFMARKET/credit_plus/sensi_mayov16.xls", skip=3)
+ec <- ec[ec$Modelo==ecu,]
+
+# matriz correlaciones
+mc <- read_excel("E:/APP_OFFMARKET/credit_plus/corr_oficiales.xls", skip=4)
+mc<- mc[,-c(1)]
+
+#ca <- read.csv2("file_ca")
+#lgd_add<- read.csv2("file_lgd")
 
 num_sim=1000000
 
 nrnd <- matrix(rnorm(0,1), nrow= num_sim, ncol=15)
 inrnd <- matrix(qnorm(rnorm(0,1)), nrow= num_sim, ncol=15)
 
-# Lectura de fichero de texto
-
-de<- read.csv2("file_csv", sep=";")
-
-# Extrae ru de analisis
 
