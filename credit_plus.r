e2a50fad26c8ceb097aca350cf988f4d150ff94d
offmarketeu <analisis@offmarket.eu>
2019-09-03 23:28:07 +0200

    Add files via upload

--- credit_plus.r
+++ credit_plus.r
@@ -13,17 +13,26 @@ fd_ru<- fd[fd$RU_ID==ur,]
 # ecuaciones minoristas
 ec <- read_excel("E:/APP_OFFMARKET/credit_plus/sensi_mayov16.xls", skip=3)
 ec <- ec[ec$Modelo==ecu,]
+ec<- ec[,-c(1:2)]
 
 # matriz correlaciones
 mc <- read_excel("E:/APP_OFFMARKET/credit_plus/corr_oficiales.xls", skip=4)
 mc<- mc[,-c(1)]
+mc[is.na(mc)]<-0
 
 #ca <- read.csv2("file_ca")
 #lgd_add<- read.csv2("file_lgd")
 
 num_sim=1000000
 
-nrnd <- matrix(rnorm(0,1), nrow= num_sim, ncol=15)
-inrnd <- matrix(qnorm(rnorm(0,1)), nrow= num_sim, ncol=15)
+nrnd <- matrix(qnorm(rnorm(0,1)), nrow= num_sim, ncol=36)
+
+
+#
+
+
+mc_ <- nrnd %*% as.matrix(mc)
+idio <- sqrt(1 - sum(ec^2))* matrix(qnorm(rnorm(0,1)), nrow= num_sim, ncol=1)
+sys_cwi <- as.matrix(ec) %*% as.matrix(t(nrnd))
 
 
